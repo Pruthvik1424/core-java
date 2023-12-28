e6ebc29c60652620268b5e3eaa6a5b3203eca48e
Pruthvik1424 <pruthvik1459@gmail.com>
2023-12-28 08:05:43 +0530

    example

---
+++ Static And nonStatic Variables/LenovoLaptop.java
@@ -0,0 +1,13 @@
+public class LenovoLaptop{
+
+          Static String BrandName;
+		  Static String Founder;
+		  Static int ManufacturingStarted;
+		  int price;
+		  String ChargingType;
+		  String Storage;
+		  String BatteryType;
+		  
+
+
+}
