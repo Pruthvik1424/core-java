d1e5e8f7b21b89628fd8bd747d03d4d9003e9b4a
Pruthvik1424 <pruthvik1459@gmail.com>
2023-12-28 08:19:14 +0530

    static

---
+++ Static And nonStatic Variables/SamsungMobile.java
@@ -0,0 +1,12 @@
+public class SamsungMobile{
+ 
+         Static String BrandName;
+		 Static String Founder;
+		 Static int ManufacturingStarted;
+		 int price;
+		 boolean IsItWaterproof;
+		 String MobileVarient;
+		 String Storage;
+		 
+
+}
