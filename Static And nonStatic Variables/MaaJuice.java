f1646e868e7d26db254b41f2407ad5e881e55a9b
Pruthvik1424 <pruthvik1459@gmail.com>
2023-12-28 08:07:07 +0530

    example

---
+++ Static And nonStatic Variables/MaaJuice.java
@@ -0,0 +1,13 @@
+public class MaaJuice{
+	
+	     Static String BrandName;
+		 Static int ManufacturingYear;
+		 Static boolean MangofruitIsUsed;
+		 int price;
+		 String  Quantity;
+		 int ExpriedDate;
+		 int ManufacturingDate;
+		 
+	
+	
+}
