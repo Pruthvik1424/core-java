1001e349e40fc056a4cb48b93a1e4fbbfeae6cc1
Pruthvik1424 <pruthvik1459@gmail.com>
2023-12-28 08:18:15 +0530

    static

---
+++ Static And nonStatic Variables/olaEV.java
@@ -0,0 +1,15 @@
+public class OlaEv {
+	
+	Static String BrandName;
+	Static String ElectricVehicle;
+	Static String Founder;
+	int price;
+	String BatteryType;
+	String BatteryCapacity;
+	String SpeedLimit;
+	
+	
+	
+	
+	
+}
