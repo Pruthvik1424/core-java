26ab200e7d5e7ae774b68efb8466d7da06220d4a
Pruthvik1424 <pruthvik1459@gmail.com>
2023-12-28 08:03:45 +0530

    example

---
+++ Static And nonStatic Variables/HeroHondaBike.java
@@ -0,0 +1,13 @@
+public class HeroHondaBike{
+
+    Static String PetrolBike;
+	Static String BrandName;
+    Static String TwoWheeler;
+    int SpeedLimit;
+	int Price;
+	String SparesParts;
+	int Rating;
+	
+
+
+}
