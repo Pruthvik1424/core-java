8636018e934bcf52e30856969e7a061f17a23725
Pruthvik1424 <pruthvik1459@gmail.com>
2023-12-28 08:20:22 +0530

    static

---
+++ Static And nonStatic Variables/Shoes.java
@@ -0,0 +1,12 @@
+public class NikeShoes{
+
+      static String brandName;
+	  static String material;
+	  static String ForMen;
+      int ShoesSize;
+	  char Quality;
+	  boolean IsitWaterproof;
+	  int Price;
+	  
+
+}
